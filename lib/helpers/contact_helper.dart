ba82ade0901d49b742cfa0c75f3fbb62e42176a9
Abigail Lima <abigailmvlima@outlook.com>
2024-08-21 10:49:01 -0300

    criei o saveContact

--- lib/helpers/contact_helper.dart
+++ lib/helpers/contact_helper.dart
@@ -38,38 +38,51 @@ class ContactHelper {
     });
   }
 
+  Future<Contact> saveContact(Contact contact) async {
+    Database dbContact = await db;
+    contact.id = await dbContact.insert(contactTable, contact.toMap());
+    return contact;
+  }
+
 }
 
 class Contact {
-  late int id;
-  late String name;
-  late String email;
-  late String phone;
-  late String img;
+   int? id;
+   String name;
+   String email;
+   String phone;
+   String img;
 
-  Contact.fromMap(Map map){
-    id = map[idColumn];
-    name = map[nameColumn];
-    email = map[emailColumn];
-    phone = map[phoneColumn];
-    img = map[imgColumn];
-  }
+   Contact({
+     this.id,
+     required this.name,
+     required this.email,
+     required this.phone,
+     required this.img,
+   });
 
-  Map toMap(){
-    Map<String, dynamic> map = {
-      nameColumn: name,
-      emailColumn: email,
-      phoneColumn: phone,
-      imgColumn: img,
-    };
-    if (id != null){
-      map[idColumn] = id;
-    }
-    return map;
-  }
+   Contact.fromMap(Map<String, dynamic> map)
+       : id = map[idColumn],
+         name = map[nameColumn] ?? '',
+         email = map[emailColumn] ?? '',
+         phone = map[phoneColumn] ?? '',
+         img = map[imgColumn] ?? '';
 
-  @override
-  String toString() {
-    return "Contact (id: $id, name: $name, email: $email, phone: $phone, img: $img) ";
-  }
+   Map<String, dynamic> toMap() {
+     final map = <String, dynamic>{
+       nameColumn: name,
+       emailColumn: email,
+       phoneColumn: phone,
+       imgColumn: img,
+     };
+     if (id != null) {
+       map[idColumn] = id;
+     }
+     return map;
+   }
+
+   @override
+   String toString() {
+     return "Contact(id: $id, name: $name, email: $email, phone: $phone, img: $img)";
+   }
 }
