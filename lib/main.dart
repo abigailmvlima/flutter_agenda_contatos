1ac5e6e2514cfdb512ed64c8b5f1c8841146043b
Abigail Lima <abigailmvlima@outlook.com>
2024-08-21 10:11:00 -0300

    primeiro commit

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MaterialApp());
+}
+
+class Home extends StatefulWidget {
+  const Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
+
